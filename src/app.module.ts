afba86a6d8b853033e6a65f45e3482b44f83a3c2
YmanolCastillo <ymanolcastillo@gmail.com>
2024-12-06 14:21:54 -0400

    Connection to ENJ server

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,33 +12,15 @@ import { ConfigModule } from '@nestjs/config';
 
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // host: 'dpg-cq11touehbks73ekt8r0-a.oregon-postgres.render.com',
-      // port: 5432,
-      // username: 'juego_proceso',
-      // password: 'bv8WrFVEZJyZZT1Cm0vfiABSD9Ocw1ZM',
-      // database: 'juego_proceso_o2nt',
-      // JWT_SECRET="no utilizar esta palabra en producción"
-      // host: process.env.POSTGRES_HOST,
-      // port: parseInt(process.env.PORT),
-      // username: process.env.POSTGRES_USERNAME,
-      // password: process.env.POSTGRES_PASSWORD,
-      // database: process.env.POSTGRES_DATABASE,
-      // TO USE LATER
-      // host: 'elproceso.enj.org',
-      // port: 5432,
-      // username: 'elprocesoenj_elprocesoenj',
-      // password: 'elprocesoenj_elprocesoenj',
-      // database: 'juego_proceso_db',
-
-      // TEMPORAL
-      host: 'dpg-csk26klds78s73962d0g-a.oregon-postgres.render.com',
+      host: '54.86.192.86',
       port: 5432,
-      username: 'juego_proceso_30m8_user',
-      password: 'rP2RS9XvnMZw9ymgcV5O80MCXyANTbkF',
-      database: 'juego_proceso_30m8',
+      username: 'user_proceso_24',
+      password: 'lN1f2i[2!82mEh9M.BeX)&q',
+      database: 'elprocesodb',
       autoLoadEntities: true,
       synchronize: true,
-      ssl: true,
+      ssl: false,
+      // ssl: true,
       // extra: {
       //   ssl:
       //     'true' === 'true'
